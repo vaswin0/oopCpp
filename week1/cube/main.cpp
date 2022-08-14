1439370e6e8ef8e99b3794ea59734506d1de1cfd
vaswin0 <vaswin0137@gmail.com>
2022-08-15 02:27:06 +0530

    main

---
+++ week1/cube/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Cube.h"
+
+
+int main() {
+
+	Cube c;
+
+	c.setLength(3.48);
+	double volume = c.getVolume();
+	std::cout << "Volume:" << volume  << std::endl;
+
+	return 0;
+
+}
