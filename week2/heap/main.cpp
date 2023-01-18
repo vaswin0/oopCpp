ce2ac336c9b7f48f87ee31b10d3240c5c7a18ebf
aswin <aswin@pop-os.localdomain>
2023-01-19 00:34:07 +0530

    new

---
+++ week2/heap/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main(){
+	int *numPtr = new int;
+
+	std::cout << "*numPtr: " << *numPtr << std::endl;
+	std::cout << "numPtr: " <<  numPtr <<std::endl;
+	std::cout << "&numPtr:" << &numPtr <<std::endl;
+
+
+
+	*numPtr = 42;
+	std::cout << "*numPtr assigned 42" << std::endl;
+
+
+	std::cout << "*numPtr:" << *numPtr << std::endl;
+	std::cout << "numPtr:" << numPtr << std::endl;
+	std::cout << "&numPtr:" << &numPtr << std::endl;
+
+	return 0;
+}
