160852c5e310905f859c9d4ddb6502b24c374312
vaswin0 <vaswin0137@gmail.com>
2023-02-11 00:28:46 +0530

    main

--- week4/towerOfHanoi1/main.cpp
+++ week4/towerOfHanoi1/main.cpp
@@ -1,34 +1,44 @@
 #include<iostream>
-#include "Cube.h"
-#include "Stack.h"
+//#include "Cube.h"
+//#include "Stack.h"
 //using polygon::Cube;
-
-#include <string>
+#include "Game.h"
+//#include <string>
 
 
 using namespace std;
 
 int main() {
-std::string r = "red";
-Cube c(10, r);
+//std::string r = "red";
+//Cube c(10);//, r);
 //std::cout<<c.length_<<std::endl;
 //std::cout<<c.color_ << std::endl;
-Stack s;
-s.stack.push_back(c);
+//Stack s;
+//s.stack.push_back(c);
 //std::cout<<s.stack[0].length_<<std::endl;
 
 //cout<<s.stack.size()<<endl;
 
 
-Cube d(30, r);
+//Cube d(30);//  , r);
 
-s.add(d);
+//s.add(d);
 
-cout<<s.stack.size()<<endl; 
 
-s.remove(d);
 
-cout<<s.stack.size()<<endl; 
+//cout<<s.stack.size()<<endl; 
+
+//s.remove(d);
+
+//cout<<s.stack.size()<<endl; 
+
+
+Game g;
+
+cout<<g.a.stack.size()<<endl;
+
+
+
 
 
 
